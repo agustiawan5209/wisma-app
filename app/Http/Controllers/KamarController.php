4a734db854557bf9c48fa67b235ef078fd364eef
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-08-04 03:57:01 +0800

    update kamarcontroller index

--- app/Http/Controllers/KamarController.php
+++ app/Http/Controllers/KamarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Kamar;
 use App\Http\Requests\StoreKamarRequest;
 use App\Http\Requests\UpdateKamarRequest;
+use Inertia\Inertia;
 
 class KamarController extends Controller
 {
@@ -13,7 +14,9 @@ class KamarController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Admin/Kamar', [
+            'kamar'=> Kamar::with(['details','tipe'])->orderBy('id','desc')->paginate(10),
+        ]);
     }
 
     /**
