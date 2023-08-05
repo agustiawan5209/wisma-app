4a4583ee82472018848a6eae39f88cd041fa6c25
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-08-05 17:17:36 +0800

    create controller model and migration Diskon, Voucher

---
+++ app/Http/Controllers/DiskonController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Diskon;
+use App\Http\Requests\StoreDiskonRequest;
+use App\Http\Requests\UpdateDiskonRequest;
+
+class DiskonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDiskonRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Diskon $diskon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Diskon $diskon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDiskonRequest $request, Diskon $diskon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Diskon $diskon)
+    {
+        //
+    }
+}
