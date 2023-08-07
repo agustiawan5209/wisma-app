9ab697ffe8248bd8e053debc69b7b96f132ed57e
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-08-07 16:18:15 +0800

    update chekout controller

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\CheckoutRequest;
-use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
